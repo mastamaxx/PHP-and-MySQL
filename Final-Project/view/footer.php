6ae39347898490c1901162c3316ae169e260e422
mastamaxx <max.j.mark@gmail.com>
2017-01-05 13:07:26 -0800

    Add files via upload

---
+++ Final-Project/view/footer.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <!--
+     * Mark Max
+     * markmax@my.smccd.edu
+     * Final
+     * CIS 380 OL
+     * File Name: footer.php
+     * Date: 2016/12/17
+     -->
+    
+    
+     <footer>
+         <p class="copyright">
+             &copy; <?php echo date("Y"); ?> Student Grades, Inc.
+         </p>
+     </footer>
+</html>
---
+++ Final-Project/view/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <!--
+     * Mark Max
+     * markmax@my.smccd.edu
+     * Final
+     * CIS 380 OL
+     * File Name: header.php
+     * Date: 2016/12/17
+    
+    
+    
+    
+    
+    <!-- the head section -->
+    <head>
+        <title>Student Grades</title>
+        <link rel="stylesheet" type="text/css" 
+              href="/CIS380_Assignment_3/Product_Manager/main.css">
+    </head>
+    
+    <!-- the body section -->
+    <body>
+        <header>
+            <h1>Student Grades</h1>
+        </header>
+    </body>
+</html>
+
+
+
